e254972c19961a9635b3c3e74c026ce7724694bf
ajumobiisaac96 <94201052+ajumobiisaac96@users.noreply.github.com>
2022-06-21 17:41:13 +0100

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,5 @@
+let first_name = "Isaac"
+let Country = "Nigeria"
+let height = "1.8 meters"
+
+console.log("HELLO, my first name is "+first_name +", I am from "+ Country +", and i am "+ height+ " tall");
